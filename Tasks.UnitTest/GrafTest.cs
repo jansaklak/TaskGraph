3e3e9de579a5f90a730d8caffc967adaefb653df
Angelo Norelli <angelo.norelli@student.uj.edu.pl>
2025-05-26 09:30:58 +0200

    złączenie ui z testami

--- Tasks.UnitTest/GrafTest.cs
+++ Tasks.UnitTest/GrafTest.cs
@@ -19,17 +19,18 @@ public class GrafTest {
     }
 
     [TestMethod]
-    [DataRow(1, 2, 5)]
-    [DataRow(11, 22, 3)]
-    [DataRow(0, 32, 1)]
-    [DataRow(5, 4, 2)]
-    [DataRow(1, 10, 13)]
-    public void Graf_BFS_ReturnsCorrectOrder(int ver1, int ver2, int ver3) {
+    [DataRow(0, 2, 5, 12)]
+    [DataRow(0, 22, 3, 4)]
+    [DataRow(0, 32, 1, 2)]
+    [DataRow(0, 4, 2, 50)]
+    [DataRow(0, 10, 13, 11)]
+    public void Graf_BFS_ReturnsCorrectOrder(int ver1, int ver2, int ver3, int ver4) {
         var graf = new Graf();
         graf.AddEdge(ver1, ver2);
         graf.AddEdge(ver2, ver3);
+        graf.AddEdge(ver2, ver4);
         var bfs = graf.BFS();
-        CollectionAssert.AreEqual(new List<int> { ver1, ver2, ver3 }, bfs);
+        CollectionAssert.AreEqual(new List<int> { ver1, ver2, ver3, ver4 }, bfs);
     }
 
     [TestMethod]
